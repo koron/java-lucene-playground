740c8c894e133ca24860c873fb666620c5e90b79
MURAOKA Taro <koron.kaoriya@gmail.com>
2022-12-22 22:36:48 +0900

    update Lucene 9.4.+

--- src/main/java/net/kaoriya/lucene_playground/Main.java
+++ src/main/java/net/kaoriya/lucene_playground/Main.java
@@ -3,9 +3,9 @@ package net.kaoriya.lucene_playground;
 import org.apache.lucene.util.CharsRef;
 import org.apache.lucene.util.IntsRef;
 import org.apache.lucene.util.IntsRefBuilder;
-import org.apache.lucene.util.fst.Builder;
 import org.apache.lucene.util.fst.CharSequenceOutputs;
 import org.apache.lucene.util.fst.FST;
+import org.apache.lucene.util.fst.FSTCompiler;
 import org.apache.lucene.util.fst.Util;
 
 public class Main {
@@ -20,16 +20,17 @@ public class Main {
     }
 
     public static void fstTrial() throws Exception {
-        System.out.println("build a FST");
+        System.out.println("build a FST:");
         // build a FST.
-        Builder<CharsRef> b = new Builder<>(FST.INPUT_TYPE.BYTE1,
+        FSTCompiler<CharsRef> c = new FSTCompiler<>(FST.INPUT_TYPE.BYTE1,
                 CharSequenceOutputs.getSingleton());
         // order to add() is too important.
-        b.add(toUTF16("bar"), new CharsRef("quux"));
-        b.add(toUTF16("foo"), new CharsRef("baz"));
-        b.add(toUTF16("foobar"), new CharsRef("waldo"));
-        FST<CharsRef> f = b.finish();
-        System.out.println("f=" + f);
+        c.add(toUTF16("bar"), new CharsRef("quux"));
+        c.add(toUTF16("foo"), new CharsRef("baz"));
+        c.add(toUTF16("foobar"), new CharsRef("waldo"));
+        System.out.println("  c=" + c);
+        FST<CharsRef> f = c.compile();
+        System.out.println("  f=" + f);
 
         System.out.println("make some queries:");
         fstQuery(f, "foo");
